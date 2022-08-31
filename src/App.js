62fff0068fdf3f5c08916e7d2f5481aadb240fa6
soyoung <thgml1444@naver.com>
2022-08-31 22:22:13 +0900

    newsData _ async-await _ useEffect로 업데이트

--- src/App.js
+++ src/App.js
@@ -5,21 +5,6 @@ import NewsList from './components/NewsList';
 
 
 const App = ()=> {
-  // const [data, setData] = useState(null)
-  // const onClick = async ()=> {
-  //   try {
-  //     const response = await axios.get(
-  //       'https://newsapi.org/v2/top-headlines?country=kr&apiKey=3ae77db5a80743df8bf855f48c84afd1'
-  //     )
-  //     .then(response =>{
-  //       setData(response.data)
-  //     })
-  //   } catch(e){
-  //     console.log(e)
-  //   }
-  // }
-  
-
   return (
     // <div>
     //   <div>
--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,18 +1,51 @@
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 import NewsItem from './NewsItem';
 
 const NewsList = ()=> {
+
+  const [articles, setArticles] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(()=>{
+    //async 사용하는 함수 선언
+    const fetchData = async ()=> {
+      setLoading(true)
+      try{
+        const response = await axios.get(
+          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=3ae77db5a80743df8bf855f48c84afd1'
+        )
+        console.log(response)
+        setArticles(response.data.articles)
+      }catch(e){
+        console.log(e)
+      }
+      // news 로딩이 끝나면 false값으로 바꿔주기
+      setLoading(false) 
+    }
+    // useEffect에서 사용할 진짜 함수!!
+    fetchData()
+  },[])
+
+  
+  // 대기중화면
+  if(loading){
+    return (<NewsListBlock>대기중</NewsListBlock>);
+  }
+  // article값이 없을때
+  if(!articles){
+    return null
+  }
   return (
     <>
       <NewsListBlock>
-        <NewsItem article={sampleArticle}/>
-        <NewsItem article={sampleArticle}/>
-        <NewsItem article={sampleArticle}/>
-        <NewsItem article={sampleArticle}/>
-        <NewsItem article={sampleArticle}/>
+        {articles.map(article => (
+          <NewsItem key={article.url} article={article}/>
+        ))}
       </NewsListBlock>
     </>
-  );
+  )
 };
 
 const NewsListBlock = styled.div`
